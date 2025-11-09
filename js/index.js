91be4de576443da59159c5b650cd58f80c04625a
TinaKovalova <kovalevatkv@gmail.com>
2025-11-09 17:28:28 +0200

    about

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ window.addEventListener("load", () => {
         if (targetElement.closest(".burger-button")) {
             e.currentTarget.classList.toggle("active-menu");
         }
+       
+        if (targetElement.tagName === "A" && e.currentTarget.classList.contains("active-menu")) {
+            e.currentTarget.classList.remove("active-menu");
+        }
           
     });
 });
