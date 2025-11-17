0cffcef5fa5466d8677250f9acc228b101a99a38
TinaKovalova <kovalevatkv@gmail.com>
2025-11-17 22:56:57 +0200

    pricelist

--- js/swipers.js
+++ js/swipers.js
@@ -40,4 +40,38 @@ window.addEventListener("load", () => {
       },
     },
   });
+
+   const pricelistSwiper = new Swiper(".pricelist__swiper.swiper", {
+     speed: 2000,
+     slidesPerView: "auto",
+     //  centeredSlides: true,
+     centerInsufficientSlides: true,
+     spaceBetween: 19,
+     initialSlide: 1,
+     grabCursor: true,
+
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+       type: "bullets",
+     },
+
+     breakpoints: {
+       // when window width is >= 320px
+       320: {
+        //  loop: true,
+        //  centeredSlides: true,
+         //  slidesPerView: 1,
+         //  spaceBetween: 10,
+       },
+       480: {
+         //  loop:true,
+         // slidesPerView: 2.5,
+       },
+       1430: {
+         loop: false,
+         centerInsufficientSlides: true,
+       },
+     },
+   });
 })
